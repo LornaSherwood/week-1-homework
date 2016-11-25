68dd2fd4d5cae62f6817e2d9e2bbf81037b8fcee
Lorna Sherwood <lornasherwood@gmail.com>
2016-11-25 14:25:08 +0000

    completed pets by breed

--- pet_shop.rb
+++ pet_shop.rb
@@ -11,11 +11,30 @@ def add_or_remove_cash(shop_info, cash_to_add)
 end
 
 def pets_sold(shop_info)
-return shop_info[:admin][:pets_sold]
+  return shop_info[:admin][:pets_sold]
 end
 
 def increase_pets_sold(shop_info, num_pets_sold)
+  return shop_info[:admin][:pets_sold] += num_pets_sold
+end
 
-return shop_info[:admin][:pets_sold] += num_pets_sold
+def stock_count(shop_info)
+  return shop_info[:pets].count
+end
+
+def pets_by_breed(pet_shop, breed_wanted)
+  new_array = []
+  
+  for pet in pet_shop[:pets]
+    if pet[:breed] == breed_wanted
+      new_array.push(breed_wanted)
+    end
+  end
+
+  return new_array
 
 end
+
+
+
+
