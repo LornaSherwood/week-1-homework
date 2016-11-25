64986e0984efa2607b682f79d8b1aa024d9fb949
Lorna Sherwood <lornasherwood@gmail.com>
2016-11-25 12:14:47 +0000

    completed add/remove cash

--- pet_shop.rb
+++ pet_shop.rb
@@ -1,9 +1,12 @@
 def pet_shop_name(shop_info)
-return shop_info.fetch(:name)
+  return shop_info.fetch(:name)
 end
 
 def total_cash(shop_info)
-
-return shop_info[:admin][:total_cash]
-
+  return shop_info[:admin][:total_cash]
 end
+
+def add_or_remove_cash(shop_info, cash_to_add)
+  return shop_info[:admin][:total_cash] += cash_to_add
+end
+
